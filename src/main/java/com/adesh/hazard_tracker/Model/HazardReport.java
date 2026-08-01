f4a8605254baf85809105e80ba84f13b9a4ebb1d
AJ <2448638@swansea.ac.uk>
2026-08-01 19:51:27 +0100

    properties, getters, setters, JPA import for PostgreSQL DBMS. Declaring primary key.

--- src/main/java/com/adesh/hazard_tracker/Model/HazardReport.java
+++ src/main/java/com/adesh/hazard_tracker/Model/HazardReport.java
@@ -1,20 +1,50 @@
 package com.adesh.hazard_tracker.Model;
 
+import jakarta.persistence.*; // JPA needed for database
 
+@Entity // Class represents the table in database
+@Table(name = "hazard_reports") // naming table in PostgreSQL
 public class HazardReport {
 
     private long id;
-    private long longitude;
-    private long latitude;
+    private double longitude;
+    private double latitude;
     private String title;
     private String description;
 
-    public HazardReport(long id, long longitude, long latitude, String title, String description){
+    public HazardReport(long id, double longitude, double latitude, String title, String description){
         this.id = id;
         this.title = title;
         this.description = description;
         this.longitude = longitude;
         this.latitude = latitude;
     }
+
+    public HazardReport() {
+
+    }
+
+    @Id // declaring the primary key for db
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // automatically generate new sequential number every time there is a new report
+    public long getId(){
+        return id;
+    }
+
+    public double getLongitude(){
+        return longitude;
+    }
+
+    public double getLatitude(){
+        return latitude;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
 
 }
