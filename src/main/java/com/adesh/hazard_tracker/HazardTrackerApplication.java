b32e14019ffcf342b58a595e09eb46434e97675e
AJ <51755236+adj06@users.noreply.github.com>
2026-08-02 23:22:55 +0100

    test

--- src/main/java/com/adesh/hazard_tracker/HazardTrackerApplication.java
+++ src/main/java/com/adesh/hazard_tracker/HazardTrackerApplication.java
@@ -3,6 +3,7 @@ package com.adesh.hazard_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// main class
 @SpringBootApplication
 public class HazardTrackerApplication {
 
