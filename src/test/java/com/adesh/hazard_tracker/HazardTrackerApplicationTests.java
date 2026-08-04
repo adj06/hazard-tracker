18f472a64b2e2d33fbb05c455860edb70ce49d2b
AJ <51755236+adj06@users.noreply.github.com>
2026-08-04 15:02:55 +0100

    dependency for testing framework

--- src/test/java/com/adesh/hazard_tracker/HazardTrackerApplicationTests.java
+++ src/test/java/com/adesh/hazard_tracker/HazardTrackerApplicationTests.java
@@ -1,7 +1,8 @@
 package com.adesh.hazard_tracker;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
 
 @SpringBootTest
 class HazardTrackerApplicationTests {
