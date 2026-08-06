678fe8e844676e295ab0f9c22ab43fb18e17a6d7
AJ <51755236+adj06@users.noreply.github.com>
2026-08-06 17:00:41 +0100

    creation of service test class for JUnit & mockito

---
+++ src/test/java/com/adesh/hazard_tracker/HazardReportServiceTest.java
@@ -0,0 +1,14 @@
+package com.adesh.hazard_tracker;
+
+import com.adesh.hazard_tracker.Model.HazardReport;
+import com.adesh.hazard_tracker.Repository.HazardReportRepository;
+import com.adesh.hazard_tracker.service.HazardReportService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+class testStatus {
+
+}
