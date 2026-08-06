922422d9949ad334b9f85087ef8c95eba23ae63c
AJ <51755236+adj06@users.noreply.github.com>
2026-08-06 21:41:19 +0100

    implementing testStatusSuccess test method using mockito

--- src/test/java/com/adesh/hazard_tracker/HazardReportServiceTest.java
+++ src/test/java/com/adesh/hazard_tracker/HazardReportServiceTest.java
@@ -9,6 +9,37 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
 class testStatus {
+
+    @Mock
+    private HazardReportRepository repository;
+
+    @InjectMocks
+    private HazardReportService service;
+
+    @Test
+
+    public void testStatusSuccess(){
+
+        Long testId = 1L;
+
+        HazardReport testReport = new HazardReport();
+        testReport.setId(testId);
+        testReport.setTitle("Test Hazard");
+        testReport.setStatus("REPORTED");
+
+        when(repository.findById(testId)).thenReturn(Optional.of(testReport));
+        when(repository.save(any(HazardReport.class))).thenAnswer(i -> i.getArguments()[0]);
+
+        HazardReport updatedReport = service.updateStatus(testId, "RESOLVED");
+        assertEquals("RESOLVED", updatedReport.getStatus(), "The status should be successfully updated to RESOLVED");
+
+    }
 
 }
