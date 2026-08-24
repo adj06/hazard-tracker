40102a7f7d11775a53934bb826c6488b134273a3
AJ <51755236+adj06@users.noreply.github.com>
2026-08-24 22:24:18 +0100

    changed data.sql file to add severity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
-INSERT INTO hazard_reports (type, description, latitude, longitude, status)
-VALUES ('POTHOLE', 'Massive pothole in road, caused damaged on my tyre', 51.52, -0.16, 'REPORTED');
+INSERT INTO hazard_reports (type, severity, description, latitude, longitude, status)
+VALUES ('POTHOLE', 'HIGH', 'Massive pothole in road, caused damaged on my tyre', 51.52, -0.16, 'REPORTED');
 
-INSERT INTO hazard_reports (type, description, latitude, longitude, status)
-VALUES ('STREETLIGHT', 'Broken streetlight, glass all over the road', 81.3, -2.07, 'UNDER_REVIEW');
+INSERT INTO hazard_reports (type, severity, description, latitude, longitude, status)
+VALUES ('STREETLIGHT', 'MEDIUM', 'Broken streetlight, glass all over the road', 81.3, -2.07, 'UNDER_REVIEW');
+
+INSERT INTO hazard_reports (type, severity, description, latitude, longitude, status)
+VALUES ('ROAD_SIGN', 'LOW', 'Lane signs wearing off on roundabout', 52.35, -0.129, 'SCHEDULED')
