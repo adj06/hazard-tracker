29ee689f1b8728ce65640bf9a6a7c37115f47052
AJ <51755236+adj06@users.noreply.github.com>
2026-08-29 17:32:02 +0100

    validation for request DTO

--- src/main/java/com/adesh/hazard_tracker/dto/HazardReportRequest.java
+++ src/main/java/com/adesh/hazard_tracker/dto/HazardReportRequest.java
@@ -2,18 +2,31 @@ package com.adesh.hazard_tracker.dto;
 
 import com.adesh.hazard_tracker.model.HazardSeverity;
 import com.adesh.hazard_tracker.model.HazardType;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 
 public class HazardReportRequest {
 
+    @NotNull(message = "Longitude can't be null")
+    @DecimalMin(value = "-180.0", message = "Longitude minimum is -180.0")
+    @DecimalMax(value = "180.0", message = "Longitude maximum is 180.0")
     private Double longitude;
 
+    @NotNull(message = "Latitude can't be null")
+    @DecimalMin(value = "-90.0", message = "Latitude minimum is -90.0")
+    @DecimalMax(value = "90.0", message = "Latitude maximum is 90.0")
     private Double latitude;
 
+    @NotNull(message = "HazardType can't be null")
     private HazardType type;
 
+    @NotNull(message = "Hazard severity can't be null")
     private HazardSeverity severity;
 
+    @NotBlank(message = "Description can't be blank")
     private String description;
 
     public HazardReportRequest(){
